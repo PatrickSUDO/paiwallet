8cab81409ffbd8106fe7aaa2d61d5f60d94c45a9
PatrickSUDO <patricksuph@gmail.com>
2024-01-28 17:26:59 +0100

    mongodb setup done

--- init-mongo.js
+++ init-mongo.js
@@ -1,7 +1,8 @@
+db = new Mongo().getDB("paiwalletdb");
 db.createUser(
     {
-        user: "paiwalletuser",
-        pwd: "example",
+        user: "psu",
+        pwd: "psu",
         roles: [
             {
                 role: "readWrite",
@@ -11,15 +12,20 @@ db.createUser(
     }
 );
 
-db = new Mongo().getDB("paiwalletdb");
-
 db.createCollection('users', { capped: false });
 
 // Add an initial user (you can add any initial data you want here)
-db.users.insert([
+db.users.insertOne([
     {
         username: "admin",
-        password: "hashed_password_here", // remember to hash your password
+        password: "password", // remember to hash your password
         email: "admin@paiwallet.com"
     }
 ]);
+
+// Show all users and databases
+var allUsers = db.getUsers();
+printjson(allUsers);
+
+var allDatabases = db.adminCommand({ listDatabases: 1 });
+printjson(allDatabases);
